38f850fdec9d21a04e46342adaf258d96172f9b4
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-02 17:37:46 +0000

    dataype

---
+++ basic/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as new version
+let name="rishav"
+let age=21
+ // there are seven type of datatype
+ // number
+ //bignit
+ //string
+ //boolean
+ //null
+ // undefined
+ //symbol =>unique
+
+ // object
+ console.log(typeof "rishav")
+ console.log(typeof 20)
+ console.log(typeof null) // object
+ console.log(typeof undefined)// undefined
---
+++ basic/variable.js
@@ -0,0 +1,16 @@
+const accountId=2206092
+let accountEmail="rk12@gmail.com"
+var accountPassword=12345
+let accountStates;
+
+accountCIty="bihar"
+
+accountEmail="aws12@gmail.com"
+accountPassword="09876"
+accountCIty="patna"
+/*
+var never use beacuse issue in block scope and functional scope
+*/
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCIty,accountStates])
