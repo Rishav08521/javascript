558c3826c9e5e90e94d121fee360889e7d9457df
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-02 18:15:26 +0000

    operation

---
+++ basic/convoperation.js
@@ -0,0 +1,17 @@
+let score="33abc"
+console.log(typeof score)
+console.log(typeof (score))
+let valurNum=Number(score)
+console.log(typeof valurNum)
+console.log(valurNum); // nan not a number
+// "33" number
+// 33abc nan not a number
+// 1= true
+// 0 =false
+// " "= false
+// "arpana" true
+// null =object
+// undefined= undefined
+
+
+
