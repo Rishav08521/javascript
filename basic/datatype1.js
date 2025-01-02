ff25719b25576a97c8fbfb8a39c767a017d9a037
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-02 19:50:08 +0000

    newdatatype

---
+++ basic/datatype1.js
@@ -0,0 +1,55 @@
+// interview type of question
+// . there 7 primitive datatype
+// 1.string                                    
+// 2.number
+// 3.null
+// 4.undefined
+// 5.boolean
+// 6.symbol
+// 7.bigInt
+// Number => number
+// String  => string
+// Boolean  => boolean
+// null  => object
+// undefined  =>  undefined
+// Symbol  =>  symbol
+// BigInt  =>  bigint
+
+//. 2. reference type or (non primitive)
+// 1.Array
+// 2.object
+// 3.function
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+const score=100;
+const valueScore=39.3;
+const isLoggedin=false;
+const outsideLogged=null;
+let userEmail;
+
+//
+// const Id=symbol('123')
+//const anotherId=symbol('123')
+//console.log(Id ===anotherId);
+const bigNumber= 123874656738472n
+//console.log(bigNumber);
+const heroes=["superman","batman","bageera"]; // array
+
+
+let myObj={
+    name:"arpana",   // object
+    age:21
+}
+
+// function
+const myFuntion=function(){
+    console.log("hello world");
+    
+}
+console.log(typeof bigNumber);
+
+
+
+
