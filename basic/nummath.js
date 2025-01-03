e4d3cf7fa491feb9f762fd783fd97fdf0bc5f801
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-03 06:55:50 +0000

    math

---
+++ basic/nummath.js
@@ -0,0 +1,33 @@
+const score=40
+console.log(score);
+
+const balance=new Number(200)
+console.log(balance);
+console.log(balance.toString.length);
+console.log(balance.toFixed(2));
+
+const otherNum=134.90
+console.log(otherNum.toPrecision(3));
+const hundre=100000
+console.log(hundre.toLocaleString());
+
+//////
+console.log(Math);
+console.log(Math.abs(-2));
+console.log(Math.round(4.3));
+console.log(Math.round(5.9));
+console.log(Math.ceil(5.9));  // upper round
+console.log(Math.floor(5.9));  // lower
+console.log(Math.min(3,7,8,4,5,9));
+console.log(Math.max(5,9,8,4,10));
+console.log(Math.random()*10);
+console.log((Math.random()*10) +1);
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min +1) )+min);
+
+
+
+
+
+
