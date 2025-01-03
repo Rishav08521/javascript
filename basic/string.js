c79fc9adf4d6635d37c6d54f04ec912607f83095
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-03 06:10:09 +0000

    string

---
+++ basic/string.js
@@ -0,0 +1,32 @@
+const name="arpana"
+const repo=50
+//console.log(name+repo+"value")
+console.log('hi my name is ${name.} and my repo count is ${ repo}' );
+const gameName=new String('education');
+console.log(gameName[0]);
+console.log(gameName.__proto_);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('a'));
+
+const newString=gameName.substring(0,5);
+console.log(newString);
+const newsString=gameName.slice(-2,5);
+console.log(newsString);
+
+const newoneString=" ravan ";
+
+console.log(newoneString);
+console.log(newoneString.trim());
+
+
+
+
+
+
+
+
+
+
+
