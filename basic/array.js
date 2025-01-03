835612f3cb614feddc5839c908c1ba0391016454
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-03 10:35:12 +0000

    array

---
+++ basic/array.js
@@ -0,0 +1,16 @@
+// slice and splice
+const array=[1,2,3,4,5,6]
+console.log('A',array);
+const mun1=array.slice(1,3)
+
+console.log(mun1);
+console.log('B',array);
+// splice
+const mn3=array.splice(1,3)
+console.log('C',array);
+console.log(mn3);
+
+
+
+
+
