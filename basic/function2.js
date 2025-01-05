26674fed9cc010c69cf4ac390d88b8ec158f40f4
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-05 08:07:07 +0000

    function2

---
+++ basic/function2.js
@@ -0,0 +1,11 @@
+function mycart(val1,val2,...num1){
+    return num1
+}
+console.log(mycart(200,400,600,800));
+
+const newArray=[200,300,400,500,600]
+function listofArray(getArray){
+    return getArray[1];
+}
+console.log (listofArray(newArray));
+
