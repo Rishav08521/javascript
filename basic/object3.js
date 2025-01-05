773dea2cb358d407c2aab1230b32e9955f6ee803
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-05 07:12:45 +0000

    objectaoi

---
+++ basic/object3.js
@@ -0,0 +1,20 @@
+const course={
+couseName:"javascript",
+coursePrice:2202,
+courseInstructor:"arpana"
+}
+//const.courseInstructor;
+const {courseInstructor}=course;
+console.log(courseInstructor);
+// decstructuring
+// const navbar = ({companay})=>{
+
+// }
+// navbar(company="hhitesh")
+// 1.api found in  jason format
+// // 2 array format
+[
+    {},
+    {},
+    {}
+]
