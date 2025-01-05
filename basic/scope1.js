e0a66bd072efaf456ca45059bb9e0e90849a6def
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-05 10:18:07 +0000

    scop1

---
+++ basic/scope1.js
@@ -0,0 +1,39 @@
+// function one(){
+//     const userName="arpana";
+//    // console.log();
+
+//     function two(){
+//         const website="weather app"; 
+//         console.log(userName);
+        
+//     }
+//     two()
+    
+    
+//}
+//one()
+
+if(true){
+    const username="simpy"
+    if(username=="simpy"){
+        const websites=" car collection";
+        console.log(username+websites);
+        
+    }
+    //console.log(websites);
+    
+}
+//console.log(username);
+
+//hosting
+console.log(num(3));
+
+function num(add){
+    return add +1
+}
+console.log(num1);
+
+const ris=function num1(sum){
+    return sum  + 2
+
+}
