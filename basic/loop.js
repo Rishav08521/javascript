64ab476a0ef48420a3b3a086247502636c246842
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-08 14:14:46 +0000

    loop

---
+++ basic/loop.js
@@ -0,0 +1,52 @@
+// for
+for(let index=0;index<=10;index++){
+    const temp=index;
+    //console.log(temp);
+    
+}
+
+for(let i=0;i<=10;i++){
+    console.log('outer loop:${i}');
+    for(let j=0;j<=10;j++){
+      console.log(i +'*'+ j + '=' + i*j);
+      
+    }
+    
+}
+
+const myArray=["pushpa2","bhageera","Marco"]
+for(let t=0;t<myArray.length;t++){
+    const elements=myArray[t];
+    console.log(elements);
+    
+} 
+// break continue
+for(let s=0;s<=20;s++){
+    console.log('value of i is ${s}');
+    
+}
+//  let index=0;
+//  while(index<=10){
+//     console.log('value index is ${index} ');
+//     index+=2;
+    
+//  }
+let s=0;
+while(s<=5){
+    console.log(`value index is ${s}`);
+    s+=2;
+    
+}
+ let index = 0;
+while (index <= 10) {
+    console.log(`value index is ${index}`);
+    index += 2;
+}
+
+const Array=["devillers","flintoff","sachin"];
+let arr=0;
+while(arr<Array.length){
+    console.log(`player is ${Array[arr]}`);
+    arr+=1;
+    
+}
