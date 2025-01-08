d527db427693b49433544f8f2aa768d61c5be0dc
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-08 12:48:54 +0000

    one basic

---
+++ basic/one.js
@@ -0,0 +1,43 @@
+// if
+const isUserlogin=true;
+const temperature=41;
+if(temperature<=41){
+  console.log("executed");
+  
+}else{
+    console.log("temperature is greater than 41");
+}
+const score=100;
+ if(score>101){
+    console.log("playing");
+    
+ }else{
+    console.log("not playing");
+    
+ }
+ const score1=200;
+ if(score1>101){
+    var power="fly"
+    console.log('user power:${power}');
+    
+ }else{
+    console.log('user power:${power}');
+    
+ }
+ const balanced=970;
+ if(balanced < 500){
+    console.log("less than 500");
+    
+ } else if(balanced<750){
+    console.log("less than 750");
+    
+ } else if(balanced< 900){
+    console.log("less than 900");
+    
+ } else if(balanced<1080){
+    console.log("leaa than 1080");
+    
+ }else{
+    console.log("less than 100");
+    
+ }
