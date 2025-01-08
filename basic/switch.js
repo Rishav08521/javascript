e48a8842fa667cd8ec0cd1e78a6df35f29242a98
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-01-08 13:22:55 +0000

    switch

---
+++ basic/switch.js
@@ -0,0 +1,57 @@
+// switch
+const month=3;
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("february");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("default");
+        
+        break;
+}
+
+// truely
+ const userEmail="rk6872663@gmail.com";
+ if(userEmail){
+    console.log("its good email");
+        
+        
+     }else{
+        console.log("not good email");
+     } 
+     /// false whose result the false
+     //false, 0,-0,bigiNT on," ",null, undefined, nan
+     // true
+     // true word , "0",'false'," ",[ ],function(){}
+     // nullish coalescing operator
+     let val;
+     val =5 ?? 10;
+     console.log(val);
+     let val1;
+     val1=null ?? 10;
+     console.log(val1);
+
+     const iceTeaPrice=100;
+     iceTeaPrice>=80  ? console.log("less than 80") : console.log("more than 80");
+     const icecoffeTea=200;
+     icecoffeTea>=150 ? console.log("less than 150") : console.log("more than 150");
+     
+     
+     
+     
+     
+     
