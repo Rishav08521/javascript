7e6600305c5c59ffb3987fe065568aa26ab6751a
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2025-05-28 07:09:02 +0000

    projectsub

---
+++ basic/guessnum.js
@@ -0,0 +1,7 @@
+let guesnum=25;
+let usernum=("guess the number");
+while(usernum!=guesnum){
+    usernum=("you enter wrong the number, guess again");
+    
+}
+console.log("congratulation, you enter the right number");
---
+++ basic/printeven.js
@@ -0,0 +1,7 @@
+//let num=100;
+for(let num=0;num<=100;num++){
+    if(num%2==0){
+       console.log("evennum",num);
+    }
+ 
+}
